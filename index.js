2661bb72f0be8b23c3ef35cc3f72466331875e19
Manoharan R <manoharanr@Manoharans-MacBook-Air.local>
2018-08-04 16:32:27 +0530

    more modular and simple routng

--- index.js
+++ index.js
@@ -7,30 +7,96 @@
 import http from 'http'
 import { greet } from './test'
 import url from 'url'
+import stringDecoder from 'string_decoder'
 
 
 //server to respond to requests
 const server = http.createServer((req, res) => {
 
     //now parsing the url to find which res is being requested
-    
+
     //get the url and parse it  req.url will be full url, sec true to call the query string 
-    const parsedUrl= url.parse(req.url,true)
+    const parsedUrl = url.parse(req.url, true)
 
     //get the path localhost:5000/quotation pathname is quotation -- untrimmed path
-    const path=parsedUrl.pathname;
+    const path = parsedUrl.pathname;
     //trims the forward slash at the last.
-    var trimmedpath=path.replace(/^\/+|\/+$/g,'')
+    const trimmedpath = path.replace(/^\/+|\/+$/g, '')
+
+    //get the query string object
+    const queryStringObject = parsedUrl.query
 
     //get the HTTP method
-    const method=req.method.toLowerCase()
+    const method = req.method.toLowerCase()
 
-    //send the response
-    res.end(greet())
+    //get the headers
+    const headers = req.headers
 
-    //log the request
-    console.log('Requested Path',trimmedpath,' Requested Method ',method);
+    //get the payload
+    const decoder = new stringDecoder.StringDecoder('utf-8')
+    let buffer = ''
+    req.on('data', (data) => buffer += decoder.write(data))
+    req.on('end', () => {
+        buffer += decoder.end()
+
+
+        //should call the appropriate handler if anything is not matching should send to not found handler
+        const choosenHandler = typeof (router[trimmedpath])!=='undefined' ? router[trimmedpath] : handlers.notFound
+
+        var data = {
+            'trimmedPath': trimmedpath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        }
+
+        //Route the request to chosen handler
+        choosenHandler(data, (sc, pl) => {
+            sc = typeof (sc) == 'number' ? sc : 200
+            pl = typeof (pl) == 'object' ? pl : {}
+            const payloadString = JSON.stringify(pl)
+            res.writeHead(sc)
+            // should send the response only after all the payloads are recieved
+            //send the response
+            res.end(payloadString)
+            //log the request
+            console.log('statusCode', sc, 'payload ', payloadString);
+        })
+
+
+
+
+    })
+
+
 })
 
 //start the server and listen on port 5000
 server.listen(5000, () => console.log("Server started and listening on Port 5000"))
+
+
+//Define handler
+const handlers = {}
+
+//sample handler
+handlers.sample = (d, c) => {
+    //call back with http status code, and call back
+    c(406, { name: 'sampleHandler' })
+}
+
+//sample handler
+handlers.notFound = (d, c) => {
+    //call back with http status code, and call back
+    c(406)
+}
+
+
+//not found handler
+
+
+//routing requests
+
+const router = {
+    "sample": handlers.sample,
+}
